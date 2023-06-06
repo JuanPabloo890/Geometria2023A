d9c172dfd6b481a8f2260c88e7ca9e764dbc519e
Juan Falconi <juan.falconi@epn.edu.ec>
2023-06-06 07:31:53 -0500

    Generacion del constructor para la variable int nldados Implementacion de getters an setters

--- src/Figuras2D.java
+++ src/Figuras2D.java
@@ -1,4 +1,19 @@
 public class Figuras2D {
+    //calcular areas y perimetros
+    int nlados;
 
+    public Figuras2D(int nlados) {
+        this.nlados = nlados;
+    }
+
+    public int getNlados() {
+        return nlados;
+    }
+
+    public void setNlados(int nlados) {
+        this.nlados = nlados;
+    }
+
+    public void areaCuadrado(){}
 
 }
